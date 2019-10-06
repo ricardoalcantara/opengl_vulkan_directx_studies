7216977efbb850ec7503f15342be87ccca8bbe09
Ricardo Alcantara <alcantarafox@yahoo.com.br>
2019-10-06 14:35:22 -0300

    Hello-Window Done

--- learnopengl.com/src/main.cpp
+++ learnopengl.com/src/main.cpp
@@ -51,6 +51,10 @@ int main()
         // -----
         processInput(window);
 
+        // rendering command here
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
